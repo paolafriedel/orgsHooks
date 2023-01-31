35c7bf4da8248d94a50809971a2af1454a32ea96
paolafriedel <paolabbsf2910@gmail.com>
2023-01-31 16:38:58 -0300

    Utilizando funções no lugar de uma classe

--- orgsHooks/src/telas/Home/index.js
+++ orgsHooks/src/telas/Home/index.js
@@ -1,8 +1,11 @@
 import React from "react";
-import {SafeAreaView, Text} from 'react-native';
-import Topo from './componentes/Topo'
+import Topo from './componentes/Topo';
+import Produtores from './componentes/Produtores';
 
 
 export default function Home(){
-    return <Topo/>
+    return <>
+        <Topo/>
+        <Produtores/>    
+    </>
 }
