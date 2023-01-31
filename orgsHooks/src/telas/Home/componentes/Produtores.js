5ae0846ae7c03c7059fb4ca3fa29ecc0d5331b7e
paolafriedel <paolabbsf2910@gmail.com>
2023-01-31 16:45:31 -0300

    Criando estados

--- orgsHooks/src/telas/Home/componentes/Produtores.js
+++ orgsHooks/src/telas/Home/componentes/Produtores.js
@@ -1,12 +1,20 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import {Text, View, Image, StyleSheet} from 'react-native';
 import { carregaProdutores } from "../../../servicos/carregaDados";
 
 export default function Produtores(){
+
+    const [titulo, setTitulo] = useState(''); // criando estado titulo
+    const [lista, setLista] = useState([]); // criando estado lista
+
     useEffect(() =>{
         const retorno = carregaProdutores();
+
+        setTitulo(retorno.titulo);
+        setLista(retorno.lista);
+
         console.log(retorno);
     }, []);
 
-    return <Text>Produtores</Text>
+    return <Text>{titulo}</Text>
 }
